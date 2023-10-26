32846ad4f1cd165b934e755e84cb537636a7393e
ArchieK9 <108101526+ArchieK9@users.noreply.github.com>
2023-10-26 15:57:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict'
+let form = document.querySelector('form')
+let btn = document.querySelector("button")
+let inputs = document.querySelectorAll('input')
+// let inputValue = inputs.value;
+let errorMsg = document.querySelectorAll(".texts")
+let errorIcon = document.querySelectorAll(".img")
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    inputs.forEach((input, index) =>{
+        const isEmpty = !input.value;
+        errorIcon[index].style.visibility = isEmpty ? 'visible' : 'hidden';
+        errorMsg[index].style.visibility = isEmpty ? 'visible' : 'hidden';
+        input.style.border = isEmpty ?"2px solid red" : ''
+
+        if (!input.type === 'email') {
+            if (!isEmpty && !isValidEmail(input.value)) {
+                input.style.color = 'red';
+                input.value = "email@example/com";
+            } else {
+                input.style.color = '';
+            }
+        }
+    } )
+});
+
+function isValidEmail(email){
+    var emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegEx.test(email);
+}
